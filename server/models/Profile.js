d3a7b5c9906fedf6d4484a503e6056a41b324cd5
Rishab Dhamija <63626748+rishabdhamija@users.noreply.github.com>
2021-06-14 17:21:45 -0400

    profile model commit 1
    added profile.js with profile model

---
+++ server/models/Profile.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+// values for avaialbility hours each week
+const availability_values = ["10" , "10+" , "20" , "20+" , "30" , "30+"]
+
+const profileSchema = new mongoose.Schema({
+    firstname : {
+        type: String,
+        required: true,
+        trim: true,
+        validate (value) {
+            if (value.length < 1) {
+                throw new Error("Please enter your firstname!!")
+            }
+        }
+    },
+    lastname : {
+        type: String,
+        required: true,
+        trim: true,
+        validate (value) {
+            if (value.length < 1) {
+                throw new Error("Please enter your lastname!!")
+            }
+        }
+    },
+    description :{
+        type: String,
+        required: true,
+        trim: true,
+        validate (value) {
+            if (value.length < 1) {
+                throw new Error("Please describe yourself!!")
+            }
+        }
+    },
+    availability : {
+        type: String,
+        required: true,
+        trim : true,
+        default: "10",
+        validate (value) {
+            // checking whether the value is one of the allowed ones in availability_values array
+            if (!availability_values.includes(value)) {
+                throw new Error("Please choose among the following values : " , availability_values);
+            }
+        }
+    }
+});
+
+
+const Profile = new mongooose.model("profile" , profileSchema);
+
+module.exports = Profile;
