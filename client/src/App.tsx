603f97d133523899c0a04fb8e9daba17941c92ab
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-06-16 21:02:29 +0600

    profile setting skeleton done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,8 +7,10 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import { AuthProvider } from './context/useAuthContext';
 import { SocketProvider } from './context/useSocketContext';
 import { SnackBarProvider } from './context/useSnackbarContext';
+import ProfileDashboard from './pages/ProfileSetup/ProfileDashboard';
 
 import './App.css';
+import Header from './components/Header/Header';
 
 function App(): JSX.Element {
   return (
@@ -23,8 +25,17 @@ function App(): JSX.Element {
                 <Route exact path="/dashboard">
                   <Dashboard />
                 </Route>
+                <Route exact path="/profiledashboard">
+                  <>
+                    <Header></Header>
+                    <ProfileDashboard></ProfileDashboard>
+                  </>
+                </Route>
                 <Route path="*">
                   <Redirect to="/login" />
+                </Route>
+                <Route exact path="/home">
+                  <h2>Home</h2>
                 </Route>
               </Switch>
             </SocketProvider>
---
+++ client/src/pages/ProfileSetup/ProfileDashboard.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { Paper } from '@material-ui/core';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: any) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+  },
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'auto 1fr',
+    padding: '5em',
+    backgroundColor: '#FAFAFB',
+  },
+  tabs: {
+    borderRight: `0px solid ${theme.palette.divider}`,
+  },
+  paper: {
+    padding: '0 7em 0 3em',
+  },
+}));
+
+export default function ProfileDashboard(): JSX.Element {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<any>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.container}>
+      <Tabs
+        orientation="vertical"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab style={{ fontSize: '14px', textAlign: 'left' }} label="Edit Profile" {...a11yProps(0)} />
+        <Tab style={{ fontSize: '14px' }} label="Profile Photo" {...a11yProps(1)} />
+        <Tab style={{ fontSize: '14px' }} label="Payment" {...a11yProps(2)} />
+        <Tab style={{ fontSize: '14px' }} label="Security" {...a11yProps(3)} />
+        <Tab style={{ fontSize: '14px' }} label="Settings" {...a11yProps(4)} />
+      </Tabs>
+      <div className={classes.paper}>
+        <Paper style={{ minHeight: '70vh' }} elevation={3}>
+          <TabPanel value={value} index={0}>
+            Edit Profile
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            Profile Photo
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            Payment
+          </TabPanel>
+          <TabPanel value={value} index={3}>
+            Security
+          </TabPanel>
+          <TabPanel value={value} index={4}>
+            Settings
+          </TabPanel>
+        </Paper>
+      </div>
+    </div>
+  );
+}
