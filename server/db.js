099ecaa18dd7e25bfed874ce3baa596c80bdeefd
Milan Verma <millan.verma@gmail.com>
2021-06-29 13:41:34 -0600

    added mongodb remote uri

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
+  const conn = await mongoose.connect(process.env.MONGO_REMOTE_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
